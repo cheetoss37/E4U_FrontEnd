6faac1c3367d23a1ea112a9a8c892a2e0c63024d
cheetoss37 <tungnm72@wru.vn>
2021-10-11 00:53:24 +0700

    add more path

--- src/constants/path.const.js
+++ src/constants/path.const.js
@@ -2,3 +2,5 @@ export const HOME = "/";
 export const LOGIN = "/login";
 export const REGISTER = "/register";
 export const ABOUT = "/about";
+export const FREE_TEST = "/free-test";
+export const OUR_EXAMS = "/our-exams";
